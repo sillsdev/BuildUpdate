76a37b44510ffc6894b1d556cf9e7a237d7e1464
Stephen McConnel <stephen_mcconnel@sil.org>
2022-02-08 10:35:42 -0700

    Update to use https instead of http for generated scripts (20220208)
    The lock file automatically updated itself.  I assume it would do the
    same for others, so included it.

--- buildupdate.rb
+++ buildupdate.rb
@@ -150,9 +150,9 @@ verbose("Options: #{$options}")
 server = $options[:server]
 $username = ENV["BUILDUPDATE_USER"] || ask("Enter #{server} username: ") { |q| q.echo = true }
 $password = ENV["BUILDUPDATE_PASSWORD"] || ask("Enter #{server} password: ") { |q| q.echo = "*" }
-rest_url = "http://#{server}/httpAuth/app/rest/10.0"
+rest_url = "https://#{server}/httpAuth/app/rest/10.0"
 rest_api = RestClient::Resource.new(rest_url, :user=>$username, :password => $password) #, :headers => { :accept => "application/json"})
-repo_url = "http://#{server}/guestAuth/repository"
+repo_url = "https://#{server}/guestAuth/repository"
 repo_api = RestClient::Resource.new(repo_url)
 
 if $options[:build_type].nil?
